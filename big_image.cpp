c28f295a3565962aab1688445d07d07ad84cbcb2
zhao xin <xinzhao@deepglint.com>
2015-06-27 17:34:20 +0800

    Reduce print. Generate using random image patch. Paramerise rows and columns.

--- big_image.cpp
+++ big_image.cpp
@@ -25,6 +25,9 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
 
 using namespace std;
 using namespace cv;
@@ -51,8 +54,8 @@ int getdir (string dir, vector<string> &files)
 
 int main(int argc, char* argv[]) {
 	printf("big image\n");
-	if (argc!=2) {
-		printf("Usage: ./big_image <path>\n");
+	if (argc!=4) {
+		printf("Usage: ./big_image <path> <rows> <columns>\n");
 		return -1;
 	}
 
@@ -69,7 +72,8 @@ int main(int argc, char* argv[]) {
 	    cout << files[i] << endl;
     }
 
-	int row_num = 2, col_num = 2;
+	int row_num = atoi(argv[2]);
+	int col_num = atoi(argv[3]);
 	int size = files.size();
 
 	Mat unit = imread(files[0]);
@@ -79,32 +83,30 @@ int main(int argc, char* argv[]) {
 	int big_width = col_num * width;
 	printf("height = %d, width = %d, big_h = %d, big_w = %d\n", height, width, big_height, big_width);
 
+	srand((int)time(0));
 	Mat big_image = Mat::zeros(big_height, big_width, CV_8UC3);
-	/*for ( int i=0; i<size; i++ ) {
-		Mat patch = imread(files[i]);
-		imshow("patch", patch);
-		waitKey(0);
-	}*/
 	for ( int y=0; y<row_num; y++ ) {
 		for ( int x=0; x<col_num; x++ ) {
-			static int i=0;
-			Mat patch = imread(files[i++%size]);
-			imshow("patch", patch);
+			Mat patch = imread(files[rand()%size]);
 			for ( int yy=0; yy<height; yy++ ) {
 				for ( int xx=0; xx<width; xx++ ) {
-					printf("y = %d, x = %d, yy = %d, xx = %d\n", y, x, yy, xx);
-					printf("index = %d\n", (y*height)*big_width + yy*xx*width + x* width + xx);
 					big_image.data[3 * ( (y*height)*big_width + yy*big_width + x*width + xx ) + 0 ] = patch.data[3*(yy*width+xx) + 0];
 					big_image.data[3 * ( (y*height)*big_width + yy*big_width + x*width + xx ) + 1 ] = patch.data[3*(yy*width+xx) + 1];
 					big_image.data[3 * ( (y*height)*big_width + yy*big_width + x*width + xx ) + 2 ] = patch.data[3*(yy*width+xx) + 2];
 				}
 			}
-			imshow("big_image", big_image);
-			waitKey(0);
 		}
 	}
 	imshow("big_image", big_image);
 	waitKey(0);
+	unsigned long long cur_time_in_microsend;
+	// get the time in micro-second
+	struct timeval start;
+	gettimeofday(&start, NULL);
+	cur_time_in_microsend = (start.tv_sec) * 1000000+ start.tv_usec;
+	char image_loc[1024];
+	sprintf(image_loc, "%lld.jpg", cur_time_in_microsend);
+	imwrite(image_loc, big_image);
 
 	return 0;
 }
